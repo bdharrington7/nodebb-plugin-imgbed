391d77d13497f778b0dddfb4c38fbef7297572af
bdharrington7 <BDHarrington7@gmail.com>
2014-01-10 23:58:53 -0800

    made regex even more efficient and more correct

--- library.js
+++ library.js
@@ -3,14 +3,14 @@
 var Imgbed = {},
 	XRegExp = require('xregexp').XRegExp,
 	extensions = ['jpg', 'jpeg', 'gif', 'png'],
-	regexStr = '(<img src="|<a href=")?(https?://[^">]*\.(' + extensions.join('|') + '))">.+<\/a>';
+	regexStr = '<a href="(https?://[^\.]+\.[^\/]+\/[^\.]+\.(' + extensions.join('|') + '))">[^<]*<\/a>';
 
 
 var regex = XRegExp(regexStr, 'gi');
 
 Imgbed.parse = function(postContent, callback){
 	if (postContent.match(regex)){
-		postContent = postContent.replace(regex, '<img src="$2" class="mine">');
+		postContent = postContent.replace(regex, '<img src="$1" class="mine">');
 	}
 	callback(null, postContent);
 }
