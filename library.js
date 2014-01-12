f35bb7c1ab09275ca170237b3d59a236ad41ad83
bdharrington7 <BDHarrington7@gmail.com>
2014-01-12 01:25:13 -0800

    Implementing a settings panel for the plugin, not working yet

--- library.js
+++ library.js
@@ -1,5 +1,18 @@
 //library.js
 
+// stuff for the admin panel
+var		fs = require('fs'),
+		path = require('path'),
+		templates = module.parent.require('../public/src/templates.js');
+
+var contants = Object.freeze({
+	"name": "Imgbed Admin Page",
+	"admin": {
+		"route": "/imgbed",
+		"icon": "icon-heart" // TODO: change this to something that makes more sense
+	}
+});
+
 var Imgbed = {},
 	XRegExp = require('xregexp').XRegExp,
 	extensions = ['jpg', 'jpeg', 'gif', 'png'],  // add capability for control panel here
@@ -15,5 +28,32 @@ Imgbed.parse = function(postContent, callback){
 	callback(null, postContent);
 }
 
+Imgbed.registerPlugin = function(custom_header, callback){
+	custom_header.plugins.push({
+		"route": contants.admin.route,
+		"icon": contants.admin.icon,
+		"name": contants.name
+	});
+}
+
+Imgbed.addRoute = function(custom_routes, callback){
+	fs.readFile(path.resolve(__dirname, "./public/templates/admin.tpl"), function (err, template){
+		custom_routes.routes.push({
+			"route": contants.admin.route,
+			"method": "get",
+			"options": function (req, res, callback) {
+				callback({
+					req: req,
+					res: res,
+					route: constants.admin.route,
+					name: constants.name,
+					content: template
+				});
+			}
+		});
+
+		callback(null, custom_routes);
+	});
+}
 
 module.exports = Imgbed;
---
+++ public/templates/admin.tpl
@@ -0,0 +1,33 @@
+<h1>ImgBed</h1>
+
+<h3>Options</h3>
+
+<form class="form">
+	<div class="form-group">
+		<label for="upload">
+			<input type="checkbox" data-field="nodebb-plugin-imgbed:options:upload" id="upload" />
+			Upload linked pictures automatically to the forum server
+		</label>
+		<div class="alert alert-warning">
+			<strong><i class="icon-warning-sign"></i> Careful!</strong>
+			<p>
+				Make sure you have enough disk space and bandwidth on your server before you enable this.
+			</p>
+		</div>
+	</div>
+	<!-- TODO: make group to choose between uploading to the server, or imgur -->
+	<div class="form-group">
+		<label for="extensions">
+			Prefix for <code>code</code> blocks
+		</label>
+		<input class="form-control" placeholder="jpeg,jpg,gif,png" type="text" data-field="nodebb-plugin-imgbed:options:extensions" id="extensions" />
+	</div>
+
+	<button class="btn btn-lg btn-primary" id="save">Save</button>
+</form>
+
+<script type="text/javascript">
+	require(['forum/admin/settings'], function(Settings) {
+		Settings.prepare();
+	});
+</script>
